e381780b9802d2b5937323fec40df555d05be392
Jeremy Herzog <jeremyaherzog@gmail.com>
2019-04-20 13:47:31 -0500

    replace default readme and titles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: "/receipt-calculator",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Receipt Calculator`,
+    description: `Gatsby site for splitting receipts`,
+    author: `@herzo175`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
